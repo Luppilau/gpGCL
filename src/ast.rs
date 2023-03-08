5d363130d95e79c8f691ff046aea009926dfa4d6
luppilau <cl@estaid.dk>
2023-03-08 16:57:04 +0100

    work

--- src/ast.rs
+++ src/ast.rs
@@ -1,11 +1,11 @@
-#[derive(Debug)]
+use std::fmt::{Debug, Error, Formatter};
+
 pub enum Expr {
     Number(i32),
     Variable(String),
     ExprOp(Box<Expr>, ExprOpcode, Box<Expr>),
 }
 
-#[derive(Debug)]
 pub enum ExprOpcode {
     Add,
     Sub,
@@ -15,20 +15,17 @@ pub enum ExprOpcode {
     Mod,
 }
 
-#[derive(Debug)]
 pub enum LogicalExpr {
     Not(Box<LogicalExpr>),
     LogicalOp(Box<LogicalExpr>, LogicalOpcode, Box<LogicalExpr>),
     LogicalExprOp(Box<Expr>, LogicalExprOpcode, Box<Expr>),
 }
 
-#[derive(Debug)]
 pub enum LogicalOpcode {
     And,
     Or,
 }
 
-#[derive(Debug)]
 pub enum LogicalExprOpcode {
     Equal,
     NotEqual,
@@ -38,7 +35,6 @@ pub enum LogicalExprOpcode {
     GreaterThanOrEq,
 }
 
-#[derive(Debug)]
 pub enum Command {
     Skip,
     Diverge,
@@ -46,21 +42,78 @@ pub enum Command {
     Assignment(String, Box<Expr>),
     RandomAssignment(String, ProbabilityDistribution),
     Sequence(Box<Command>, Box<Command>),
-    ProbabilisticChoice(Box<Command>, Probability, Box<Command>),
     NondeterministicChoice(Box<Command>, Box<Command>),
-    Conditional(Box<LogicalExpr>, Box<Command>, Box<Command>),
+    ProbabilisticChoice(Box<Command>, Probability, Box<Command>),
+    IfElse(Box<LogicalExpr>, Box<Command>, Box<Command>),
     While(Box<LogicalExpr>, Box<Command>),
 }
 
-#[derive(Debug)]
 pub enum Probability {
     Probability(f32),
 }
 
-#[derive(Debug)]
 pub enum ProbabilityDistribution {
     Normal(i32, i32),
     Uniform(i32),
     LogNormal(i32),
     Exponential(i32),
+}
+
+impl Debug for Expr {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        use self::Expr::*;
+        match &*self {
+            Number(n) => write!(fmt, "{:?}", n),
+            Variable(s) => write!(fmt, "{:?}", s),
+            ExprOp(ref l, op, ref r) => write!(fmt, "({:?} {:?} {:?})", l, op, r),
+        }
+    }
+}
+impl Debug for LogicalExpr {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        use self::LogicalExpr::*;
+        match &*self {
+            Not(b) => write!(fmt, "!{:?}", b),
+            LogicalOp(ref l, op, ref r) => write!(fmt, "({:?} {:?} {:?})", l, op, r),
+            LogicalExprOp(ref l, op, ref r) => write!(fmt, "({:?} {:?} {:?})", l, op, r),
+        }
+    }
+}
+
+impl Debug for ExprOpcode {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        use self::ExprOpcode::*;
+        match *self {
+            Add => write!(fmt, "+"),
+            Sub => write!(fmt, "-"),
+            Mul => write!(fmt, "*"),
+            Div => write!(fmt, "/"),
+            Monus => write!(fmt, ":-"),
+            Mod => write!(fmt, "%"),
+        }
+    }
+}
+
+impl Debug for LogicalOpcode {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        use self::LogicalOpcode::*;
+        match *self {
+            And => write!(fmt, "&&"),
+            Or => write!(fmt, "||"),
+        }
+    }
+}
+
+impl Debug for LogicalExprOpcode {
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        use self::LogicalExprOpcode::*;
+        match *self {
+            Equal => write!(fmt, "=="),
+            NotEqual => write!(fmt, "!="),
+            LessThan => write!(fmt, "<"),
+            LessThanOrEq => write!(fmt, "<="),
+            GreaterThan => write!(fmt, ">"),
+            GreaterThanOrEq => write!(fmt, ">="),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,35 +11,107 @@ fn main() -> Result<(), std::io::Error> {
 
 #[test]
 fn expression_parser() {
-    assert!(grammar::ExprParser::new().parse("22").is_ok());
-    assert!(grammar::ExprParser::new().parse("22 + 22").is_ok());
-    assert!(grammar::ExprParser::new().parse("22 * 22").is_ok());
-    assert!(grammar::ExprParser::new().parse("a + a * a").is_ok());
-    assert!(grammar::ExprParser::new().parse("a * a + a").is_ok());
-    assert!(grammar::ExprParser::new().parse("aAaAa").is_ok());
-    assert!(grammar::ExprParser::new().parse("(22)").is_ok());
+    let parser = grammar::exprParser::new();
 
-    assert!(grammar::ExprParser::new().parse("22 * + 22").is_err());
-    assert!(grammar::ExprParser::new().parse("22.1").is_err());
-    assert!(grammar::ExprParser::new().parse("a A").is_err());
-    assert!(grammar::ExprParser::new().parse("a1 + a").is_err());
+    assert!(parser.parse("22").is_ok());
+    assert!(parser.parse("variablename").is_ok());
+    assert!(parser.parse("22 * 22").is_ok());
+    assert!(parser.parse("22 / 22").is_ok());
+    assert!(parser.parse("22 % 22").is_ok());
+    assert!(parser.parse("22 + 22").is_ok());
+    assert!(parser.parse("22 - 22").is_ok());
+    assert!(parser.parse("22 :- 22").is_ok());
+    assert!(parser.parse("a + a * a").is_ok());
+    assert!(parser.parse("a * a + a").is_ok());
+    assert!(parser.parse("(22)").is_ok());
+
+    assert!(parser.parse("22 * + 22").is_err());
+    assert!(parser.parse("22.1").is_err());
+    assert!(parser.parse("a A").is_err());
+    assert!(parser.parse("a1 + a").is_err());
+}
+
+#[test]
+fn expression_associativity_parser() {
+    let parser = grammar::exprParser::new();
+
+    let expr = parser.parse("1 * 2 + 3").unwrap();
+    assert_eq!(&format!("{:?}", expr), "((1 * 2) + 3)");
+
+    let expr = parser.parse("1 / 2 - 3").unwrap();
+    assert_eq!(&format!("{:?}", expr), "((1 / 2) - 3)");
+
+    let expr = parser.parse("1 % 2 :- 3").unwrap();
+    assert_eq!(&format!("{:?}", expr), "((1 % 2) :- 3)");
+
+    let expr = parser.parse("1 * (2 + 3)").unwrap();
+    assert_eq!(&format!("{:?}", expr), "(1 * (2 + 3))");
 }
 
 #[test]
 fn logical() {
-    assert!(grammar::LogicalExprParser::new().parse("22 < 21").is_ok());
-    assert!(grammar::LogicalExprParser::new().parse("!22 < 22").is_ok());
-    assert!(grammar::LogicalExprParser::new().parse("!!22 < 22").is_ok());
-    assert!(grammar::LogicalExprParser::new()
-        .parse("!22 < 22 && 22 < 22")
+    let parser = grammar::logical_exprParser::new();
+
+    assert!(parser.parse("!22 < 21").is_ok());
+    assert!(parser.parse("1 < 1 && 1 < 1").is_ok());
+    assert!(parser.parse("1 < 1 || 1 < 1").is_ok());
+    assert!(parser.parse("22 < 21").is_ok());
+    assert!(parser.parse("22 <= 21").is_ok());
+    assert!(parser.parse("22 > 22").is_ok());
+    assert!(parser.parse("22 >= 22").is_ok());
+    assert!(parser.parse("22 == 22").is_ok());
+    assert!(parser.parse("22 != 22").is_ok());
+
+    assert!(parser.parse("22 !!= 22").is_err());
+    assert!(parser.parse("22 < !22").is_err());
+    assert!(parser.parse("22 < 22 < 22").is_err());
+}
+
+#[test]
+
+fn logical_associativity_parser() {
+    let parser = grammar::logical_exprParser::new();
+
+    let expr = parser.parse("!1>2 && 1>2").unwrap();
+    assert_eq!(&format!("{:?}", expr), "!((1 > 2) && (1 > 2))");
+
+    let expr = parser.parse("(!1>2) && 1>2").unwrap();
+    assert_eq!(&format!("{:?}", expr), "(!(1 > 2) && (1 > 2))");
+
+    let expr = parser.parse("!(1>2) && (1>2)").unwrap();
+    assert_eq!(&format!("{:?}", expr), "!((1 > 2) && (1 > 2))");
+
+    let expr = parser.parse("1>2 && 1>2 || 1>2").unwrap();
+    assert_eq!(&format!("{:?}", expr), "(((1 > 2) && (1 > 2)) || (1 > 2))");
+}
+
+#[test]
+fn command() {
+    let parser = grammar::commandParser::new();
+
+    assert!(parser.parse("skip").is_ok());
+    assert!(parser.parse("diverge").is_ok());
+    assert!(parser.parse("tick(1)").is_ok());
+    assert!(parser.parse("x:=1").is_ok());
+    assert!(parser.parse("x := normal()").is_ok());
+    assert!(parser.parse("x := uniform()").is_ok());
+    assert!(parser.parse("x := lognormal()").is_ok());
+    assert!(parser.parse("x := exponential()").is_ok());
+    assert!(parser.parse("skip ; skip").is_ok());
+    assert!(parser.parse("{skip} [] {skip}").is_ok());
+    assert!(parser.parse("{skip} [0.1] {skip}").is_ok());
+    assert!(parser.parse("if (a==0) { skip } else { skip }").is_ok());
+    assert!(parser.parse("while (a > b) { skip }").is_ok());
+    assert!(parser
+        .parse(
+            "if (x>=y) {
+                {skip} [] {skip}
+            } else {
+                diverge
+            }
+            "
+        )
         .is_ok());
 
-    assert!(grammar::LogicalExprParser::new()
-        .parse("!(22 < 22 || 22 < 22)")
-        .is_ok());
-
-    assert!(grammar::LogicalExprParser::new().parse("22 < !22").is_err());
-    assert!(grammar::LogicalExprParser::new()
-        .parse("22 < 22 < 22")
-        .is_err());
+    assert!(parser.parse("tick(0.1)").is_err());
 }
