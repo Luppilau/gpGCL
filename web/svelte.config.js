092f9e9acc6e34a64e02c797b6b9cee35617c17c
luppilau <cl@estaid.dk>
2023-06-25 13:43:42 +0200

    removed port mapping

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -18,10 +18,6 @@ const config = {
   // See https://kit.svelte.dev/docs/adapters for more information about adapters.
   adapter: adapter(),
  },
- preview: {
-  port: 8080,
-  strictPort: false,
- },
 };
 
 export default config;
