ea4d9acd32c0f1d812a99c6cc9e585b5e9788d2d
luppilau <cl@estaid.dk>
2023-06-25 21:04:41 +0200

    fixed traces of cegis impl

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,145 +5,38 @@ extern crate lalrpop_util;
 mod cors;
 mod handler;
 
-use std::{fs::File, io::Write, process::Command as CommandProcess};
-
 use grammar::ast::*;
 use grammar::transform::Transform;
 use grammar::transpile::Transpile;
+use grammar::visit::Visit;
 use handler::{ParseError, RequestHandler};
 use serde::{Deserialize, Serialize};
 
 pub struct CustomHandler;
 impl RequestHandler for CustomHandler {
     // TODO: Implement custom transpilation here
-    fn transpile(&self, ast: Command) -> String {
-        struct Transpiler;
-        impl grammar::transpile::Transpile for Transpiler {
-            fn transpile_logical_expr_opcode(&mut self, i: LogicalExprOpcode) -> String {
-                match i {
-                    LogicalExprOpcode::Equal => "=".to_string(),
-                    LogicalExprOpcode::LessThan => "<".to_string(),
-                    LogicalExprOpcode::LessThanOrEq => "<=".to_string(),
-                    _ => unreachable!("Invalid logical expression opcode"),
-                }
-            }
-
-            fn transpile_logical_opcode(&mut self, i: LogicalOpcode) -> String {
-                match i {
-                    LogicalOpcode::And => "&".to_string(),
-                    LogicalOpcode::Or => "||".to_string(),
-                }
-            }
-
-            fn transpile_prob_dist_uniform(&mut self, i: ProbDistUniform) -> String {
-                format!(
-                    "unif({}, {})",
-                    grammar::transpile::transpile_lit_number(self, i.min),
-                    grammar::transpile::transpile_lit_number(self, i.max)
-                )
-            }
-        }
-
-        Transpiler.transpile_command(ast)
-    }
+    // fn transpile(&self, ast: Command) -> String {
+    //     struct Transpiler;
+    //     impl grammar::transpile::Transpile for Transpiler {
+    //     }
+    //     Transpiler.transpile_command(ast)
+    // }
 
     // TODO: Implement custom transformation here
-    fn transform(&self, ast: Command) -> Command {
-        struct Transformer;
-        impl grammar::transform::Transform for Transformer {
-            fn transform_logical_expr_op(&mut self, i: LogicalExprOp) -> LogicalExprOp {
-                match i.op {
-                    LogicalExprOpcode::GreaterThan => LogicalExprOp {
-                        op: LogicalExprOpcode::LessThan,
-                        left: i.right,
-                        right: i.left,
-                    },
-                    LogicalExprOpcode::GreaterThanOrEq => LogicalExprOp {
-                        op: LogicalExprOpcode::LessThanOrEq,
-                        left: i.right,
-                        right: i.left,
-                    },
-
-                    _ => grammar::transform::transform_logical_expr_op(self, i),
-                }
-            }
-        }
-
-        Transformer.transform_command(ast)
-    }
+    // fn transform(&self, ast: Command) -> Command {
+    //     struct Transformer;
+    //     impl grammar::transform::Transform for Transformer {
+    //     }
+    //     Transformer.transform_command(ast)
+    // }
 
     // TODO: Implement custom visitation here
-    fn visit(&self, ast: &Command) -> Result<(), String> {
-        struct Visitor {
-            errors: Vec<String>,
-            variable_declarations: Vec<String>,
-        }
-
-        impl grammar::visit::Visit for Visitor {
-            fn visit_assignment(&mut self, i: &Assignment) {
-                if !self.variable_declarations.contains(&i.name.name) {
-                    self.variable_declarations.push(i.name.name.clone())
-                } else {
-                    self.errors.push(format!(
-                        "Variable {} is declared more than once",
-                        i.name.name.clone()
-                    ));
-                }
-            }
-
-            fn visit_random_assignment(&mut self, i: &RandomAssignment) {
-                if !self.variable_declarations.contains(&i.name.name) {
-                    self.variable_declarations.push(i.name.name.clone())
-                } else {
-                    self.errors.push(format!(
-                        "Variable {} is declared more than once",
-                        i.name.name.clone()
-                    ))
-                }
-            }
-            fn visit_diverge(&mut self, _i: &Diverge) {
-                self.errors
-                    .push("Diverge operation not supported".to_string());
-            }
-            fn visit_nondeterministic_choice(&mut self, _i: &NondetChoice) {
-                self.errors
-                    .push("Nondeterministic choice not supported".to_string());
-            }
-            fn visit_logical_expr_opcode(&mut self, i: &LogicalExprOpcode) {
-                match i {
-                    LogicalExprOpcode::NotEqual => self
-                        .errors
-                        .push("Not equal operator not supported".to_string()),
-                    _ => (),
-                }
-            }
-            fn visit_prob_dist(&mut self, i: &ProbDist) {
-                match i {
-                    ProbDist::Normal(_) => self
-                        .errors
-                        .push("Normal distribution not supported".to_string()),
-                    ProbDist::Uniform(_) => {}
-                    ProbDist::LogNormal(_) => self
-                        .errors
-                        .push("Lognormal distribution not supported".to_string()),
-                    ProbDist::Exponential(_) => self
-                        .errors
-                        .push("Exponential distribution not supported".to_string()),
-                }
-            }
-        }
-        let mut checker = Visitor {
-            errors: vec![],
-            variable_declarations: vec![],
-        };
-
-        grammar::visit::visit_command(&mut checker, ast);
-        if checker.errors.len() > 0 {
-            return Err(checker.errors.join("\n"));
-        } else {
-            return Ok(());
-        };
-    }
+    // fn visit(&self, ast: &Command) -> Result<(), String> {
+    //     struct Visitor;
+    //     impl grammar::visit::Visit for Visitor {}
+    //     Visitor.visit_command(ast);
+    //     Ok()
+    // }
 }
 
 #[derive(Serialize)]
@@ -199,49 +92,18 @@ fn execute(input: String) -> String {
     // TODO: Transform only if necessary
     let transformed_source = CustomHandler.transform(parsed_source.unwrap());
 
-    let mut assignment_checker = AssignmentVisitor {
-        variable_declarations: vec![],
-        errors: vec![],
-    };
-
-    grammar::visit::visit_command(&mut assignment_checker, &transformed_source);
-
     // Transpile input
     let transpiled_source = &CustomHandler.transpile(transformed_source);
 
-    let transpiled_source = format!(
-        "{}{}",
-        assignment_checker
-            .variable_declarations
-            .iter()
-            .fold(String::new(), |acc, item| {
-                acc + &format!("nat {};\n", item)
-            }),
-        transpiled_source
-    );
-
     // TODO: Call tool with transpiled
 
-    let mut file = File::create("temp.imp.pgcl").unwrap();
-    file.write_all(transpiled_source.as_bytes()).unwrap();
-
-    let mut process = CommandProcess::new("sh");
-    process.arg("-c").arg(format!(
-        "poetry run python3 -m cegispro2.cmd ../temp.imp.pgcl {}",
-        request.args
-    ));
-
-    process.current_dir("cegispro2");
-
-    let output = process.output().unwrap();
-
-    let respose = ExecutionResponse {
-        result: std::str::from_utf8(&output.stdout).unwrap().to_string(),
-        errors: vec![std::str::from_utf8(&output.stderr).unwrap().to_string()],
+    let response = ExecutionResponse {
+        result: transpiled_source.to_string(),
+        errors: vec![],
     };
 
     // Respond with result
-    serde_json::to_string(&respose).unwrap()
+    serde_json::to_string(&response).unwrap()
 }
 
 #[launch]
@@ -249,23 +111,4 @@ fn rocket() -> _ {
     rocket::build()
         .attach(cors::CORS)
         .mount("/", routes![validate, execute])
-}
-
-struct AssignmentVisitor {
-    variable_declarations: Vec<String>,
-    errors: Vec<String>,
-}
-
-impl grammar::visit::Visit for AssignmentVisitor {
-    fn visit_assignment(&mut self, i: &Assignment) {
-        if !self.variable_declarations.contains(&i.name.name) {
-            self.variable_declarations.push(i.name.name.clone())
-        }
-    }
-
-    fn visit_random_assignment(&mut self, i: &RandomAssignment) {
-        if !self.variable_declarations.contains(&i.name.name) {
-            self.variable_declarations.push(i.name.name.clone())
-        }
-    }
 }
